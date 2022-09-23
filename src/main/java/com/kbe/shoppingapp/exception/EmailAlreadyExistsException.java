5664c50073a89e178f861d607ededc94c45364af
Fabian Vogt <fabian.vogt@sap.com>
2022-09-23 16:43:46 +0200

    add spring items

--- src/main/java/com/kbe/shoppingapp/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.kbe.shoppingapp.exception;
-
-public class EmailAlreadyExistsException extends Exception {
-    public EmailAlreadyExistsException() {
-        super("Email already exists!");
-    }
-}
--- src/main/java/com/kbe/shoppingapp/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.kbe.shoppingapp.exception;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String msg) {
-        
-    }
-}
