3fac657e538415ffb63a8496f4cd3fe790b43427
Fabian Vogt <fabian.vogt@sap.com>
2022-09-23 20:55:16 +0200

    configure CORS

--- src/main/java/com/kbe/shoppingapp/ShoppingApplication.java
+++ src/main/java/com/kbe/shoppingapp/ShoppingApplication.java
@@ -15,14 +15,4 @@ public class ShoppingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-      return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-          registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
-        }
-      };
-    }
 }
