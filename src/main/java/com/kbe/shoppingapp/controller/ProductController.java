81e838efaa40b0471c3ea26c95e871fbe6fb5a04
Fabian Vogt <fabian.vogt@sap.com>
2022-09-23 21:04:58 +0200

    configure CORS

--- src/main/java/com/kbe/shoppingapp/controller/ProductController.java
+++ src/main/java/com/kbe/shoppingapp/controller/ProductController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
+@CrossOrigin
 class ProductController {
 
   @Autowired
