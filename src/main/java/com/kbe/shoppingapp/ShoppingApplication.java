b1acb07f5e2156adcfb74983dbed3383d5bb8819
Fabian Vogt <fabian.vogt@sap.com>
2022-09-23 20:44:06 +0200

    configure CORS

--- src/main/java/com/kbe/shoppingapp/ShoppingApplication.java
+++ src/main/java/com/kbe/shoppingapp/ShoppingApplication.java
@@ -2,6 +2,9 @@ package com.kbe.shoppingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
@@ -11,5 +14,15 @@ public class ShoppingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+        }
+      };
     }
 }
