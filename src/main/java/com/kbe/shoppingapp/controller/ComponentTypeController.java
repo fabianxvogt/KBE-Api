2b5d8d184b86e7c1454a086ca326869bd0154c72
Fabian Vogt <fabian.vogt@sap.com>
2022-09-24 12:34:50 +0200

    CORS config

--- src/main/java/com/kbe/shoppingapp/controller/ComponentTypeController.java
+++ src/main/java/com/kbe/shoppingapp/controller/ComponentTypeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 class ComponentTypeController {
--- src/main/java/com/kbe/shoppingapp/controller/UserController.java
+++ src/main/java/com/kbe/shoppingapp/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
+@CrossOrigin
 class UserController {
 
   @Autowired
